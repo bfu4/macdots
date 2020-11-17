a396c854978f1fcfddd53cd72efb9ebcf14588c1
bfu4 <bellafusari@vivaldi.net>
2020-11-17 23:37:34 +0000

    temp fix

---
+++ ubersicht/temp/bar.jsx
@@ -0,0 +1,24 @@
+import styles from "./lib/styles.jsx";
+
+const style = {
+  background: "#83ccab",
+  cursor: "default",
+  userSelect: "none",
+  zIndex: "-1",
+  width: "15%",
+  height: "30px",
+  position: "fixed",
+  overflow: "hidden",
+  top: "4px",
+  right: "4px",
+  left: "4px",
+  borderRadius: "20px"
+};
+
+export const refreshFrequency = 1000000;
+
+export const render = ({ output }) => {
+  return <div style={style} />;
+};
+
+export default null;
---
+++ ubersicht/temp/lib/styles.jsx
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    fg: "rgba(255,255,255,0.8)",
+    dim: "rgba(255,255,255,0.6)",
+    bg: "#76668e",
+    red: "#ff8700",
+    accent: "#5fafaf"
+  },
+  fontSize: "11px",
+  lineHeight: "20px",
+  fontFamily: "'Iosevka Custom', 'SF Mono', monospace"
+}
